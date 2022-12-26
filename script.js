a03865a32bcbb4d433c24663ad6b7e0893119179
Jegadeesh waran <109015243+Jegadeesh2@users.noreply.github.com>
2022-12-26 19:53:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Elements
+let deleteBtnEle = document.getElementById('delete-btn');
+let listEle = document.getElementById('list')
+
+
+
+
+deleteBtnEle.addEventListener('click', function(){
+listEle.style.display='none';
+})
